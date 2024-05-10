74f8061d438d5f63d1b75cc40554150d2fca032c
hoangnguyen-iut <166522013+hoangnguyen-iut@users.noreply.github.com>
2024-05-10 16:16:09 +0700

    Update main.py
    doc: logic

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+### Khởi tạo và cài đặt trò chơi:
 
+
+### Hàm và biến cần thiết:
+### Logic chính
+# Xử lý sự kiện
+# Đảm bảo con rắn không thể di chuyển ngược lại ngay lập tức
+# Di chuyển con rắn
+# Cơ chế tăng kích thước của con rắn
+# Xuất hiện thức ăn trên màn hình
+# Đồ họa
+# Điều kiện kết thúc trò chơi
+# Cập nhật và hiển thị màn hình
+# Điều chỉnh tốc độ cập nhật
+### Xử lý sự kiện bấm phím và phát âm thanh:
+# Xử lý sự kiện nhấn phím
+# Phát âm thanh khi nhấn phím
+
+# Kết thúc trò chơi
