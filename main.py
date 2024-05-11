919e38634fe5451a86ad89d88e723a92a875f27f
hoangnguyen-iut <166522013+hoangnguyen-iut@users.noreply.github.com>
2024-05-11 09:16:10 +0700

    Update main.py
    feat : main code

--- main.py
+++ main.py
@@ -71,5 +71,84 @@ score = 0
 ### Xử lý sự kiện bấm phím và phát âm thanh:
 # Xử lý sự kiện nhấn phím
 # Phát âm thanh khi nhấn phím
+# Logic chính
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        # Khi một phím được nhấn xuống
+        elif event.type == pygame.KEYDOWN:
+            # W -> Lên; S -> Xuống; A -> Trái; D -> Phải
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                change_to = 'UP'
+                beep_sound.play()
+
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                change_to = 'DOWN'
+                beep_sound.play()
+
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                change_to = 'LEFT'
+                beep_sound.play()
+
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                change_to = 'RIGHT'
+                beep_sound.play()
+
+            # Esc -> Tạo sự kiện để thoát trò chơi
+            if event.key == pygame.K_ESCAPE:
+                pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+
+    # Di chuyển con rắn
+    if direction == 'UP':
+        snake_pos[1] -= 10
+    if direction == 'DOWN':
+        snake_pos[1] += 10
+    if direction == 'LEFT':
+        snake_pos[0] -= 10
+    if direction == 'RIGHT':
+        snake_pos[0] += 10
+
+    # Cơ chế tăng kích thước của con rắn
+    snake_body.insert(0, list(snake_pos))
+    if snake_pos[0] == food_pos[0] and snake_pos[1] == food_pos[1]:
+        score += 1
+        food_spawn = False
+    else:
+        snake_body.pop()
+
+    # Xuất hiện thức ăn trên màn hình
+    if not food_spawn:
+        food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
+    food_spawn = True
+
+    # Đồ họa
+    game_window.fill(black)
+    for pos in snake_body:
+        # Cơ thể con rắn
+        pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
+
+    # Thức ăn của con rắn
+    pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+
+    # Điều kiện kết thúc trò chơi
+    # Rơi ra khỏi biên
+    if snake_pos[0] < 0 or snake_pos[0] > frame_size_x-10:
+        game_over()
+    if snake_pos[1] < 0 or snake_pos[1] > frame_size_y-10:
+        game_over()
+    # Chạm vào cơ thể của con rắn
+    for block in snake_body[1:]:
+        if snake_pos[0] == block[0] and snake_pos[1] == block[1]:
+            game_over()
+
+    show_score(1, white, 'consolas', 20)
+    # Cập nhật lại màn hình trò chơi
+    pygame.display.update()
+    # Tốc độ cập nhật
+    fps_controller.tick(difficulty)
+
 
 # Kết thúc trò chơi
